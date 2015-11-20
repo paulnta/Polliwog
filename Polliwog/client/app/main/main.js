c2f9d77ae32c68357bf17d493b85ec95c04d56ca
paulnta <paul.ntawuruhunga@heig-vd.ch>
2015-11-20 23:20:02 +0100

    clean project with angular-fullstack, ui-router

---
+++ Polliwog/client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('polliwogApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
