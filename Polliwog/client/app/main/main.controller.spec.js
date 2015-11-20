ba1e0c81d8551a858495606715eb43fa6bee520d
paulnta <paul.ntawuruhunga@heig-vd.ch>
2015-11-20 23:38:39 +0100

    removed sample code

--- Polliwog/client/app/main/main.controller.spec.js
+++ Polliwog/client/app/main/main.controller.spec.js
@@ -6,24 +6,12 @@ describe('Controller: MainCtrl', function () {
   beforeEach(module('polliwogApp'));
   beforeEach(module('socketMock'));
 
-  var MainCtrl,
-      scope,
-      $httpBackend;
-
   // Initialize the controller and a mock scope
-  beforeEach(inject(function (_$httpBackend_, $controller, $rootScope) {
-    $httpBackend = _$httpBackend_;
-    $httpBackend.expectGET('/api/things')
-      .respond(['HTML5 Boilerplate', 'AngularJS', 'Karma', 'Express']);
+  beforeEach(function () {
 
-    scope = $rootScope.$new();
-    MainCtrl = $controller('MainCtrl', {
-      $scope: scope
-    });
-  }));
+  });
 
-  it('should attach a list of things to the scope', function () {
-    $httpBackend.flush();
-    expect(scope.awesomeThings.length).toBe(4);
+  it('should be logic', function () {
+    expect(true).toBe(true);
   });
 });
