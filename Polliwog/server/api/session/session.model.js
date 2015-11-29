8167db97e1d8a0bb55d5a87f3bcc4361ea4ccfd1
yibnl <yassin.kammoun@heig-vd.ch>
2015-11-29 12:10:23 +0100

    Update session model

--- Polliwog/server/api/session/session.model.js
+++ Polliwog/server/api/session/session.model.js
@@ -4,9 +4,16 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var SessionSchema = new Schema({
-  name: String,
-  info: String,
-  active: Boolean
+  key: { type: String, unique: true },
+  name: { type: String, trim: true, required: true, maxlength: 30 },
+  description: { type: String, trim: true, required: true, maxlength: 120 },
+  creationDate: { type: Date, default: Date.now },
+  isPrivate: { type: Boolean, default: false },
+  speaker: { type: Schema.ObjectId, ref: 'User', required: true },
+  users: [{ type: Schema.ObjectId, ref: 'User' }],
+  moods: [{ type: Schema.ObjectId, ref: 'Mood' }],
+  polls: [{ type: Schema.ObjectId, ref: 'Poll' }],
+  resources: [{ type: Schema.ObjectId, ref: 'Resource' }]
 });
 
 module.exports = mongoose.model('Session', SessionSchema);
