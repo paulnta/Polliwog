a0f4b8f48469ef28d23928b13b0f2b87804123b1
yibnl <yassin.kammoun@heig-vd.ch>
2015-11-29 12:04:42 +0100

    Add API session endpoint

--- Polliwog/server/routes.js
+++ Polliwog/server/routes.js
@@ -10,6 +10,7 @@ var path = require('path');
 module.exports = function(app) {
 
   // Insert routes below
+  app.use('/api/sessions', require('./api/session'));
   app.use('/api/users', require('./api/user'));
   app.use('/api/polls/:poll_id/participations/:participation_id/answers', require('./api/answer'));
   app.use('/api/polls/:poll_id/participations', require('./api/participation'));
