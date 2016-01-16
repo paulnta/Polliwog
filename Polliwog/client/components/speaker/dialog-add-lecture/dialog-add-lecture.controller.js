7e4637b005a543660b5c9c55c94755c3be96103a
yibnl <yassin.kammoun@heig-vd.ch>
2016-01-16 14:15:24 +0100

    Add lecture creation

--- Polliwog/client/components/speaker/dialog-add-lecture/dialog-add-lecture.controller.js
+++ Polliwog/client/components/speaker/dialog-add-lecture/dialog-add-lecture.controller.js
@@ -1,10 +1,9 @@
 'use strict';
 
 angular.module('polliwogApp')
-  .controller('AddLectureCtrl', function ($scope, $mdDialog) {
+  .controller('AddLectureCtrl', function ($scope, $mdDialog, Lecture) {
   	$scope.lecture = {};
   	$scope.joinUrl = 'http://polliwog.com/join'
-  	
   	$scope.visibilities = ('Public:false;Private:true').split(';').map(function (v) {
   		var args = v.split(':');
   		return {
@@ -14,15 +13,19 @@ angular.module('polliwogApp')
   	});
 
   	$scope.create = function () {
-
+      var lecture = new Lecture($scope.lecture);
+      lecture.$save({}, function (lecture) {
+        $scope.lecture = lecture;
+      });  
   	};
 
   	$scope.cancel = function () {
-  		
+  		Lecture.delete({id: $scope.lecture._id});
+      $mdDialog.cancel();
   	};
 
   	$scope.finished = function () {
-
+      $mdDialog.hide();
   	};
 
   });
