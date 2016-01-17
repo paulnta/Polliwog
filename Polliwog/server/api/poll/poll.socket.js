886af7cad001784098309a21f74fdb1828f5050b
paulnta <paul.ntawuruhunga@heig-vd.ch>
2016-01-17 05:56:59 +0100

    socket poll:updated for speaker

--- Polliwog/server/api/poll/poll.socket.js
+++ Polliwog/server/api/poll/poll.socket.js
@@ -7,7 +7,7 @@
 var Poll = require('./poll.model'),
   Question = require('../question/question.model');
 
-var speakerSocket;
+var speakerSockets = {};
 exports.register = function(socket) {
   Poll.schema.post('save', function (doc) {
     onSave(socket, doc);
@@ -18,20 +18,18 @@ exports.register = function(socket) {
 
   socket.on('poll:start', function (data) {
 
-
-    speakerSocket  = socket;
     /**
      * Le speaker passe la clé de la session, c'est plus simple pour le serveur car
      * nous devons transmettre le poll à tout les utilisateurs connectés à la room ayant cette clé comme nom.
      */
     var key = data.key;
+    speakerSockets[key] = socket;
 
     /**
     *  Le poll en question
     */
     var poll = data.poll;
 
-    // ici en envoi bien le poll à tout les utilisateurs connecté à la room
     socket.to(key).emit('poll:start', poll);
   });
   /**
@@ -40,24 +38,23 @@ exports.register = function(socket) {
    */
   socket.on('poll:vote', function (data) {
     // find the question related to the choice answered
+    var pollId;
     Question.findById(data.question, function (err, question) {
         if(!err){
-
           // update answer for this choice
           var choice = question.choices.id(data.choice);
           choice.answer_count += (data.state ? 1 : -1);
           question.save(function (err) {
             console.log(question.choices);
           });
+
+          // notify the speaker related to the session key
+          var speakerSocket = speakerSockets[data.key];
+          speakerSocket.emit('poll:updated', {pollId : question.poll});
         }
     });
-
-    // notify the speaker
-    speakerSocket.emit('poll:updated', poll);
   });
-
 };
-
 
 
 function onSave(socket, doc, cb) {
